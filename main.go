11d6b800dbc7647b6b56e077f1e57a0bd9b28fdd
Nitish Kumar <nitishkr1729@gmail.com>
2023-01-17 16:50:23 +0530

    Patch

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
@@ -18,9 +17,7 @@ func handler(ctx context.Context, event events.SQSEvent) error {
 	filesystem.InitS3Client()
 
 	for _, msg := range event.Records {
-		file, _ := json.MarshalIndent(msg.Body, "", "")
-
-		reader := bytes.NewReader(file)
+		reader := bytes.NewReader([]byte(msg.Body))
 
 		_, err = lib.PutFile(msg.MessageId+".json", reader, reader.Size(), types.ObjectCannedACLPublicRead)
 		log.Printf("Error in Uploading File: %v", err)
